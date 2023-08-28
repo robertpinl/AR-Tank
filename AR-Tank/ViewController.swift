933158e28dc6cd8c660253d3628938ca767e01bb
Robert Pinl <robert.pinl@icloud.com>
2023-08-28 18:30:46 +1200

    Initial Commit

---
+++ AR-Tank/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AR-Tank
+//
+//  Created by Robert P on 28.08.2023.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
