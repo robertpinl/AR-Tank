7c9e7ed0624ac1f6c966215562199bc52fda4426
Robert Pinl <robert.pinl@icloud.com>
2023-08-28 19:43:08 +1200

    Add ar view and basic controls

--- AR-Tank/ViewController.swift
+++ AR-Tank/ViewController.swift
@@ -10,15 +10,63 @@ import RealityKit
 
 class ViewController: UIViewController {
     
+    var tankAnchor: TinyToyTank._TinyToyTank?
+    var isActionPlaying: Bool = false
+
     @IBOutlet var arView: ARView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+
+        tankAnchor = try! TinyToyTank.load_TinyToyTank()
+        arView.scene.anchors.append(tankAnchor!)
+        tankAnchor!.cannon?.setParent(tankAnchor!.tank, preservingWorldTransform: true)
+        tankAnchor?.actions.actionComplete.onAction = { _ in
+          self.isActionPlaying = false
+        }
     }
+    
+    @IBAction func rightTapped(_ sender: UIButton) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+
+        tankAnchor!.notifications.tankRight.post()
+    }
+    
+    @IBAction func leftTapped(_ sender: UIButton) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+
+        tankAnchor!.notifications.tankLeft.post()
+    }
+    
+    @IBAction func forwardTapped(_ sender: UIButton) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+
+        tankAnchor!.notifications.tankForward.post()
+    }
+    
+    @IBAction func fireTapped(_ sender: UIButton) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+
+        tankAnchor!.notifications.cannonFire.post()
+    }
+    
+    @IBAction func turretLeftTapped(_ sender: UIButton) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+
+        tankAnchor!.notifications.turretLeft.post()
+    }
+    
+    @IBAction func turrentRightTapped(_ sender: UIButton) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+
+        tankAnchor!.notifications.turretRight.post()
+    }
+    
+    
 }
